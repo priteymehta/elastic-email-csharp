90f5c74318c32818345b342bab76a5de2a1c044e
pritey <priteymehta9@gmail.com>
2022-05-11 13:57:57 +0530

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        
+        public static string USERNAME = "USERNAME";
+        public static string API_KEY = "APIKEY";
+
+        public static void Main(string[] args)
+        {
+            string from = "FROM_EMAIL";
+            string fromName = "Your Company Name";
+            string to = "TOEMAILS";
+            string subject = "Your Subject";
+            string bodyHtml = "<h1>Html Body</h1>";
+            string bodyText = "Text Body";
+
+            string result = SendEmail(to, subject, bodyText, bodyHtml, from, fromName);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        public static string SendEmail(string to, string subject, string bodyText, string bodyHtml, string from, string fromName)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                NameValueCollection values = new NameValueCollection();
+                values.Add("username", USERNAME);
+                values.Add("api_key", API_KEY);
+                values.Add("from", from);
+                values.Add("from_name", fromName);
+                values.Add("subject", subject);
+                if (bodyHtml != null)
+                    values.Add("body_html", bodyHtml);
+                if (bodyText != null)
+                    values.Add("body_text", bodyText);
+                values.Add("to", to);
+
+                byte[] response = client.UploadValues("https://api.elasticemail.com/mailer/send", values);
+                return Encoding.UTF8.GetString(response);
+
+            }
+            catch(Exception ex)
+            {
+                return "";
+            }         
+        }
+    }
+}
+    
+
---
+++ WebApplicationEmailDemo/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace WebApplicationEmailDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            try
+            {
+                var fileStream = new FileStream(Server.MapPath("~/Content/invoice.html"), FileMode.Open, FileAccess.Read);
+                var Renderer = new IronPdf.HtmlToPdf();
+                var PDF = Renderer.RenderHTMLFileAsPdf(Server.MapPath("~/Content/invoice.html"));
+                string datetimestamp = "~/Content/" + DateTime.Now.ToString() + ".pdf";
+                PDF.SaveAs(datetimestamp);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+           
+           
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+      
+    }
+}
